2228b6658758a7976af7d157772ddc0ee128b1a9
Tanmay-92 <tanmaykumallick@gmail.com>
2021-07-21 23:55:22 +0530

    Adding the UC-6 of linked list

--- LinkedList_uc6.java
+++ LinkedList_uc6.java
@@ -1,25 +1,21 @@
 package LinkedList_Problems;
 import java.util.LinkedList;
-
-public class LinkedList_uc5 {
-	public static void main(String[] args) {
+public class LinkedList_uc6 {
 	
+	
+	public static void main(String[] args) {
 		LinkedList<Integer> LinkList = new LinkedList<Integer>();
 
+		
 		LinkList.add(56);
 		LinkList.add(70);
 		LinkList.add(1, 30);
 
-		
-		System.out.println("Initiall LinkedList  " + LinkList);
+		System.out.println("Initial LinkedList  " + LinkList);
 
-	
-		System.out.println("Deleting element from the LinkedList = " + LinkList.pop());
+		System.out.println("Removing the last element in the LinkedList : " + LinkList.removeLast());
 
-	
 		System.out.println("Final LinkedList : " + LinkList);
 	}
 
 }
-
-
