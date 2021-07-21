c21a21bb68dfbaea1f8867a72fd6ecc9bd44a7c7
Tanmay-92 <tanmaykumallick@gmail.com>
2021-07-21 14:38:10 +0530

    Appending the Linked LIst

---
+++ LinkedListUC3.java
@@ -0,0 +1,16 @@
+package LinkedList_Problems;
+import java.util.LinkedList;
+
+public class LinkedList_uc3 {
+	
+	
+	public static void main(String[] args) {	
+		New1LinkedList linklist=new New1LinkedList();
+		linklist.Append(70);
+		linklist.Append(30);
+		linklist.Append(56);
+		System.out.println("LinkedLists Are :");
+		linklist.print();	
+	}
+
+}
