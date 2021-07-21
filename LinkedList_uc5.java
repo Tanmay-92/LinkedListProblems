a48ce882a1053463116ec7010e77908cff439838
Tanmay-92 <tanmaykumallick@gmail.com>
2021-07-21 23:43:21 +0530

    Adding the UC-5 of linked list

--- LinkedList_uc5.java
+++ LinkedList_uc5.java
@@ -1,18 +1,23 @@
 package LinkedList_Problems;
 import java.util.LinkedList;
 
-public class LinkedList_uc4 {
-	
+public class LinkedList_uc5 {
 	public static void main(String[] args) {
 	
 		LinkedList<Integer> LinkList = new LinkedList<Integer>();
 
-	
 		LinkList.add(56);
 		LinkList.add(70);
 		LinkList.add(1, 30);
-		System.out.println(LinkList);
 
+		
+		System.out.println("Initiall LinkedList  " + LinkList);
+
+	
+		System.out.println("Deleting element from the LinkedList = " + LinkList.pop());
+
+	
+		System.out.println("Final LinkedList : " + LinkList);
 	}
 
 }
