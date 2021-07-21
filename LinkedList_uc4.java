53477a2e861be4bb68d05bf812d22452f8194890
Tanmay-92 <tanmaykumallick@gmail.com>
2021-07-21 22:17:50 +0530

    Adding the UC-4 OF linked List

---
+++ LinkedList_uc4.java
@@ -0,0 +1,20 @@
+package LinkedList_Problems;
+import java.util.LinkedList;
+
+public class LinkedList_uc4 {
+	
+	public static void main(String[] args) {
+	
+		LinkedList<Integer> LinkList = new LinkedList<Integer>();
+
+	
+		LinkList.add(56);
+		LinkList.add(70);
+		LinkList.add(1, 30);
+		System.out.println(LinkList);
+
+	}
+
+}
+
+
