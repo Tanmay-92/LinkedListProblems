15fd5c51590662a419aba9a1ce05b37d31a09a64
Tanmay-92 <tanmaykumallick@gmail.com>
2021-07-21 13:55:40 +0530

    Creating a Linked List

---
+++ LinkedListUC1.java
@@ -0,0 +1,76 @@
+package LinkedList_Problems;
+
+public class LinkedList_uc1 {
+	   class Node {
+	        int data;
+	        Node Next;
+
+	        public Node(int data){
+	            this.data=data;
+	            this.Next=null;
+	        }
+	    }
+
+	    public Node head=null;
+	    public Node tail=null;
+
+	    public void addNode(int data) { 
+	        Node newNode=new Node(data);
+	        if (head==null) {
+	            head=newNode;
+	            tail=newNode;
+	        }
+	        else
+	            tail.Next=newNode;
+	            tail=newNode;
+	    }
+
+	    public void addAtStart(int data) { 
+	        Node newNode = new Node(data);
+	        if(head == null) {
+	            head = newNode;
+	            tail = newNode;
+	        }
+	        else {
+	            Node temp = head;
+	            head = newNode;
+	            head.Next = temp;
+	        }
+	    }
+
+	    public void addAtEnd(int data) { 
+	        Node newNode = new Node(data);
+	        if(head == null) {
+	            head = newNode;
+	            tail = newNode;
+	        }
+	        else {
+	            tail.Next = newNode;
+	            tail = newNode;
+	        }
+	    }
+
+	    public void displayList() {
+	        Node current=head;
+	        if (head==null){
+	            System.out.println("list is empty");
+	        }
+	        else {
+	            System.out.println("singly linked list: ");
+	            while (current != null) {
+	                System.out.println(current.data + "");
+	                current = current.Next;
+	            }
+	        }
+	    }
+
+public static void main(String[] args) {
+	LinkedList_uc1 list = new LinkedList_uc1();
+
+   list.addAtEnd(56);
+   list.addAtEnd(30);
+   list.addAtEnd(70);
+
+   list.displayList();
+}
+
